e0de1fb94906fb01d0c19320a5f03109edd345c5
neolao <contact@neolao.com>
2010-11-22 00:44:59 +0100

    style

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require dirname(__FILE__).'/../bootstrap.php';
 $application = Application::getInstance();
+$tabCount = count($application->tabs);
 
 ?><!DOCTYPE html>
 <html>
@@ -15,20 +16,31 @@ $application = Application::getInstance();
             <nav role="primary navigation">
                 <ul>
                 <?php
-                $tabIndex = 0;
-                foreach ($application->tabs as $tab) {
-                    echo '<li><a href="#tab', $tabIndex, '">', $tab->name, '</a></li>', "\n";
-                    $tabIndex++;
+                for ($index = 0; $index < $tabCount; $index++) {
+                    $tab = $application->tabs[$index];
+                    if ($index === 0) {
+                        echo '<li class="selected">';
+                    } else {
+                        echo '<li>';
+                    }
+                    echo '<a href="#tab', $index, '">', $tab->name, '</a></li>', "\n";
                 }
                 ?>
                 </ul>
             </nav>
         </header>
-        <?php foreach ($application->tabs as $tab) { ?>
-        <article>
-            <h1><?php echo $tab->name; ?></h1>
-        </article>
-        <?php } ?>
+        <?php
+        for ($index = 0; $index < $tabCount; $index++) {
+            $tab = $application->tabs[$index];
+            if ($index === 0) {
+                echo '<article class="selected">';
+            } else {
+                echo '<article>';
+            }
+            echo '<h1>', $tab->name, '</h1>';
+            echo '</article>';
+        }
+        ?>
     </body>
 </html>
 
