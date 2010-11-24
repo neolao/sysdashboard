d2af13bd27878ecee1d3ff8ee8960e2a281f968d
neolao <contact@neolao.com>
2010-11-24 01:52:23 +0100

    Bootstrap modules

---
+++ classes/Core/Module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * A module
+ */
+class Core_Module extends Core_GetterSetter
+{
+    /**
+     * Module name
+     *
+     * @var string
+     */
+    public $name;
+
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ classes/Module/Text.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Module to display a text
+ */
+class Module_Text extends Core_Module
+{
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+}
