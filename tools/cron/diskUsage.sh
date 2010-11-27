d7083d4e52e6188845f472eea8a1442b03861339
neolao <contact@neolao.com>
2010-11-27 17:57:00 +0100

    Add a tool to update the disk usage

---
+++ tools/cron/diskUsage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 
+# Example:
+# diskUsage.sh sda1 login:password http://my.dashboard/api/module/foobar/data
+
+scriptPath="$( readlink -f "$( dirname "$0" )" )/$( basename "$0" )"
+currentDirectory=`dirname $scriptPath`
+restClient=$currentDirectory/../restClient.sh
+disk=$1
+auth=$2
+url=$3
+
+
+diskUsage=`df | grep sda1 | awk '{F=NF-1 ; print $F}' | tr -d '%'`
+
+$restClient $auth set $url "$diskUsage"
